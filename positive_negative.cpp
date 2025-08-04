531db780f651871284816f5cb96e7d8fac2b741e
Tanjim Ahamed <m.tanjim999@gmail.com>
2025-08-05 00:35:14 +0600

    Update positive_negative.cpp

--- positive_negative.cpp
+++ positive_negative.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
 using namespace std;
-int main(){
+
+int main() {
     int x;
 
-    cout<<"Enter the two number:";
-    cin>>x;
-    if(x>0){
-        cout<<"This is a positive number.."<<endl;
+    cout << "Enter a number: ";
+    cin >> x;
 
-    }else if(x<0){
-        cout<<"This is a negative number.."<<endl;
-    }else{
-        cout<<"This the number Zero..'0'.."<<endl;
+    if (x > 0) {
+        cout << "This is a positive number." << endl;
+    } else if (x < 0) {
+        cout << "This is a negative number." << endl;
+    } else {
+        cout << "The number is zero." << endl;
     }
-
-
 
     return 0;
 }
+
