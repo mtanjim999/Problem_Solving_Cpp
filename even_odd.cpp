cd7386ad8b002d42fbe4a1c5051851d9182e1e93
Tanjim Ahamed <m.tanjim999@gmail.com>
2025-08-05 00:15:01 +0600

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num;
+  cout << "Inter any number:";
+  cin >> num;
+  if(num % 2 == 0){
+    cout << "This is an EVEN number.." << endl;
+
+  }else{
+    cout << "This is an ODD number.."<< endl;
+  };
+
+  return 0;
+}
